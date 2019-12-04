b6156d4ca2b6dda55e371315663c8fcf26cf5011
AnnaFinnerty <algfinnerty@gmail.com>
2019-12-04 13:37:05 -0700

    fixing git issue

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -143,14 +143,7 @@ class HomeBase extends Component {
         <Modal >
           <Modal.Header>Select a Photo</Modal.Header>
           <Modal.Content image>
-            <Modal.Description>
-              <Header>Default Profile Image</Header>
-              <p>
-                We've found the following gravatar image associated with your e-mail
-                address.
-              </p>
-              <p>Is it okay to use this photo?</p>
-            </Modal.Description>
+            
           </Modal.Content>
         </Modal>
       </React.Fragment>
